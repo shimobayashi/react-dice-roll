9e0d753a5ce37c0dab9a29f8a6d62501a7058903
shimobayashi <akimasashimobayashi@gmail.com>
2022-03-02 19:33:09 +0900

    とりあえず2d6とか打ってSubmitするとダイスを振った結果を教えてくれるようにした。

--- src/App.js
+++ src/App.js
@@ -1,5 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class InputDiceForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+  
+  handleSubmit(event) {
+    const regexp = /^(\d+)d(\d+)$/;
+    const match = regexp.exec(this.state.value);
+    if (match && match.length >= 2) {
+      const number = parseInt(match[1]);
+      const surfaces = parseInt(match[2]);
+      let sum = 0;
+      for (let i = 0;i < number;i++) {
+        sum += Math.floor(surfaces * Math.random()) + 1;
+      }
+
+      alert(sum);
+    }
+
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Dice:
+          <input type="text" value={this.state.value} onChange={this.handleChange} placeholder="2d6" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    )
+  }
+}
 
 function App() {
   return (
@@ -17,6 +61,7 @@ function App() {
         >
           Learn React
         </a>
+        <InputDiceForm />
       </header>
     </div>
   );
