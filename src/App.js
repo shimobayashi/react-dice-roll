2d3dbdee77ef0fbcfdb0eb6367bede9b3e84149b
shimobayashi <akimasashimobayashi@gmail.com>
2022-03-08 14:56:26 +0900

    仮想リストに2d6の結果を出すようにした。フォームと連動してないし、値も表示毎に変わってしまっている。

--- src/App.js
+++ src/App.js
@@ -1,6 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { FixedSizeList as List } from 'react-window';
+
+function rollDice(text) {
+  const regexp = /^(\d+)d(\d+)$/;
+  const match = regexp.exec(text);
+
+  if (match && match.length >= 2) {
+    const number = parseInt(match[1]);
+    const surfaces = parseInt(match[2]);
+    let sum = 0;
+    for (let i = 0;i < number;i++) {
+      sum += Math.floor(surfaces * Math.random()) + 1;
+    }
+
+    return sum;
+  }
+
+  return undefined;
+}
+
+const Row = ({ index, style }) => (
+  <div style={style}>Row {index} {rollDice("2d6")}</div>
+);
+
+const DiceList = () => (
+  <List
+    height={150}
+    itemCount={1000}
+    itemSize={35}
+    width={300}
+  >
+    {Row}
+  </List>
+);
 
 class InputDiceForm extends React.Component {
   constructor(props) {
@@ -16,16 +50,8 @@ class InputDiceForm extends React.Component {
   }
   
   handleSubmit(event) {
-    const regexp = /^(\d+)d(\d+)$/;
-    const match = regexp.exec(this.state.value);
-    if (match && match.length >= 2) {
-      const number = parseInt(match[1]);
-      const surfaces = parseInt(match[2]);
-      let sum = 0;
-      for (let i = 0;i < number;i++) {
-        sum += Math.floor(surfaces * Math.random()) + 1;
-      }
-
+    const sum = rollDice(this.state.value);
+    if (sum) {
       alert(sum);
     }
 
@@ -49,19 +75,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <InputDiceForm />
+        <DiceList></DiceList>
       </header>
     </div>
   );
